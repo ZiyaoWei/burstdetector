cc62b4d199753a37c445ef1a4000e7fda0509a84
Ziyao Wei <ziyao.wei.wzy@gmail.com>
2018-07-18 23:01:04 -0400

    Stop using rolling and compute the correct start time

--- burstDetector.py
+++ burstDetector.py
@@ -7,37 +7,30 @@ def render(table, params):
     if not date_column_name:
         return table
 
-    import pandas as pd
-    import pandas.tseries.offsets as offsets
+    timedelta_units = ['seconds', 'minutes', 'hours', 'days', 'weeks']
+    timedelta_unit = timedelta_units[interval_unit]
+    timedelta = pd.Timedelta(**{timedelta_unit: interval_length})
 
-    offset_units = [
-        offsets.Second,  # sec
-        offsets.Minute,  # min
-        offsets.Hour,    # hour
-        offsets.Day,     # day
-        offsets.Week     # week
-    ]
-    offset_unit = offset_units[interval_unit]
-    offset = offset_unit(interval_length)
-
-    # Preprocess the dataframe for `rolling`:
-    # 1) sort the index to monotonically increasing
-    # 2) Add a column of 1s so rolling works
-    timestamps = pd.DataFrame(index=table[date_column_name]).sort_index()
-    timestamps['count'] = [1] * timestamps.shape[0]
-
-    bursts = timestamps.rolling(offset, min_periods=trigger_threshold).count()
-    bursts['is_burst'] = bursts['count'].isnull()
+    timestamps = pd.to_datetime(table[date_column_name]).dropna().sort_values()
+    bursts = pd.DataFrame({
+        'start': timestamps.shift(trigger_threshold - 1),
+        'end': timestamps,
+    })
+    bursts['is_burst'] = (bursts['end'] - bursts['start']) < timedelta
 
     # https://stackoverflow.com/questions/40802800
-    bursts['burst_start'] = bursts['is_burst'] != bursts['is_burst'].shift()
-    bursts = bursts.dropna()
-    bursts['burst_id'] = bursts['burst_start'].cumsum()
-    bursts = (bursts
-              .groupby('burst_id')
-              .apply(lambda df: pd.Series([
-                  df.index[0],
-                  df.index[-1],
-                  df['count'][0] + df.shape[0] - 1])))
-    bursts.columns = ['start', 'end', 'num_events']
-    return bursts
+    bursts = bursts[:100]
+    bursts['new_burst'] = bursts['is_burst'] & (bursts['is_burst'].shift() != True)
+    print(bursts.to_string())
+    bursts = bursts[bursts['is_burst']]
+    bursts['burst_id'] = bursts['new_burst'].cumsum()
+    bursts = bursts[:100]
+    # print(bursts)
+    merged_bursts = (bursts
+        .groupby('burst_id')
+        .apply(lambda df: pd.Series([
+            df['start'].iloc[0],
+            df['end'].iloc[-1],
+            trigger_threshold + df.shape[0] - 1])))
+    merged_bursts.columns = ['start', 'end', 'num_events']
+    return merged_bursts
