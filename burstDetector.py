d6b3d8a8bb44abf5f69bc9fb5bd187fffb5dff77
Ziyao Wei <ziyao.wei.wzy@gmail.com>
2018-08-12 00:27:53 -0400

    Fix merging bursts

--- burstDetector.py
+++ burstDetector.py
@@ -11,26 +11,29 @@ def render(table, params):
     timedelta_unit = timedelta_units[interval_unit]
     timedelta = pd.Timedelta(**{timedelta_unit: interval_length})
 
-    timestamps = pd.to_datetime(table[date_column_name]).dropna().sort_values()
-    bursts = pd.DataFrame({
-        'start': timestamps.shift(trigger_threshold - 1),
-        'end': timestamps,
-    })
-    bursts['is_burst'] = (bursts['end'] - bursts['start']) < timedelta
+    timestamps = pd.to_datetime(table[date_column_name]).dropna().sort_values().reset_index(drop=True)
+    bursts_start = timestamps.shift(trigger_threshold - 1)
+
+    is_end_of_burst = pd.concat(((timestamps - bursts_start) < timedelta, pd.Series([False] * (trigger_threshold - 1))))
+
+    last_burst_index = (is_end_of_burst * np.arange(len(is_end_of_burst))).replace(0, np.nan).fillna(method='pad')
+
+    since_last_burst = pd.Series(np.arange(len(last_burst_index))) - last_burst_index.reset_index(drop=True)
+    in_burst = since_last_burst.shift(-(trigger_threshold - 1))[:len(timestamps)] < trigger_threshold
 
     # https://stackoverflow.com/questions/40802800
-    bursts = bursts[:100]
-    bursts['new_burst'] = bursts['is_burst'] & (bursts['is_burst'].shift() != True)
-    print(bursts.to_string())
-    bursts = bursts[bursts['is_burst']]
+    bursts = pd.DataFrame({
+        'in_burst': in_burst,
+        'timestamp': timestamps,
+    })
+    bursts['new_burst'] = bursts['in_burst'] & (bursts['in_burst'].shift() != True)
+    bursts = bursts[bursts['in_burst']]
     bursts['burst_id'] = bursts['new_burst'].cumsum()
-    bursts = bursts[:100]
-    # print(bursts)
     merged_bursts = (bursts
         .groupby('burst_id')
         .apply(lambda df: pd.Series([
-            df['start'].iloc[0],
-            df['end'].iloc[-1],
-            trigger_threshold + df.shape[0] - 1])))
+            df['timestamp'].iloc[0],
+            df['timestamp'].iloc[-1],
+            df.shape[0] - 1])))
     merged_bursts.columns = ['start', 'end', 'num_events']
     return merged_bursts
